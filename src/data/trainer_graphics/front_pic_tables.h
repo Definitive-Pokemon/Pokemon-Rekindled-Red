c61d70018f35d29e2e9dcdbf64c7a9a284b2dabb
ElwinBran <elwinbran@gmail.com>
2024-09-02 17:31:49 +0200

    Filling more Celio trainer Data

--- src/data/trainer_graphics/front_pic_tables.h
+++ src/data/trainer_graphics/front_pic_tables.h
@@ -161,6 +161,7 @@ const struct MonCoords gTrainerFrontPicCoords[] =
     {.size = 8, .y_offset = 1},
     {.size = 8, .y_offset = 1},
     {.size = 8, .y_offset = 1},
+    {.size = 8, .y_offset = 1},
 };
 
 const struct CompressedSpriteSheet gTrainerFrontPicTable[] =
