85a4b20b140e85bf85c102b33a267efc9c698af0
ElwinBran <elwinbran@gmail.com>
2024-09-02 17:11:22 +0200

    Celio sprite extern definition added

--- include/graphics.h
+++ include/graphics.h
@@ -2594,6 +2594,7 @@ extern const u32 gTrainerFrontPic_SisAndBro[];
 extern const u32 gTrainerFrontPic_ProfessorOak[];
 extern const u32 gTrainerFrontPic_Daisy[];
 extern const u32 gTrainerFrontPic_Anabel[];
+extern const u32 gTrainerFrontPic_Celio[];
 extern const u32 gTrainerFrontPic_RSBrendan2[];
 extern const u32 gTrainerFrontPic_RSMay2[];
 extern const u32 gTrainerFrontPic_Red[];
