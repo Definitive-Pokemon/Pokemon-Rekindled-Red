21702015f861010c181a183dcfd82869e17a9fc9
ElwinBran <elwinbran@gmail.com>
2024-11-27 18:08:56 +0100

    Elms Lab healing respawn

--- src/heal_location.c
+++ src/heal_location.c
@@ -101,6 +101,11 @@ void SetWhiteoutRespawnWarpAndHealerNpc(struct WarpData * warp)
             warp->y = 11;
             VarSet(VAR_MAP_SCENE_TRAINER_TOWER, 0);
         }
+        else if (sWhiteoutRespawnHealCenterMapIdxs[healLocationIdx - 1][0] == MAP_GROUP(ELMS_LAB) && sWhiteoutRespawnHealCenterMapIdxs[healLocationIdx - 1][1] == MAP_NUM(ELMS_LAB))
+        {
+            warp->x = 5;
+            warp->y = 5;
+        }
         else
         {
             warp->x = 7;
