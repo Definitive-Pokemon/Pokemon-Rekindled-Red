512a65e3a65e1c8ac45e26597c16fdd4d8d13317
Thirteen1355 <dertienvijfenvijftig@gmail.com>
2024-12-19 17:48:57 +0100

    Hide ball before it should show up

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1304,7 +1304,7 @@
 #define FLAG_MAXIMUS_HIDE_LUGIA               0x4E9
 #define FLAG_MAXIMUS_SILVER_HIDE               0x4EA
 #define FLAG_HIDE_SILVER_SCIENTIST               0x4EB
-#define FLAG_0x4EC               0x4EC
+#define FLAG_HIDE_FING_BALL               0x4EC
 #define FLAG_0x4ED               0x4ED
 #define FLAG_0x4EE               0x4EE
 #define FLAG_0x4EF               0x4EF
