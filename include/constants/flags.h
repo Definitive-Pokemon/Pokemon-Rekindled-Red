83d21b9fe4a154d750ffcc7b2e310eafd3e407b8
Thirteen1355 <dertienvijfenvijftig@gmail.com>
2024-12-28 20:02:02 +0100

    Fixed Maximus not moving to final script

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1308,7 +1308,7 @@
 #define FLAG_LUGIA_FLEE               0x4ED
 #define FLAG_HIDE_NAVEL_POLICE               0x4EE
 #define FLAG_MAXIMUS_GONE               0x4EF
-#define FLAG_0x4F0               0x4F0
+#define FLAG_TO_END               0x4F0
 #define FLAG_0x4F1               0x4F1
 #define FLAG_0x4F2               0x4F2
 #define FLAG_0x4F3               0x4F3
