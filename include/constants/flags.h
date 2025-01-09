e17363d38be4c566ce59f58adebc4c6def5b7108
Thirteen1355 <dertienvijfenvijftig@gmail.com>
2025-01-09 14:09:10 +0100

    Update to Celebi event; implemented things similar to the Legendary Beast encounters

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1312,7 +1312,7 @@
 #define FLAG_DEFEATED_MAXIMUS               0x4F1
 #define FLAG_ELM_DONE               0x4F2
 #define FLAG_ANABEL_DONE               0x4F3
-#define FLAG_0x4F4               0x4F4
+#define FLAG_HIDE_LOST_PSYCHIC               0x4F4
 #define FLAG_0x4F5               0x4F5
 #define FLAG_0x4F6               0x4F6
 #define FLAG_0x4F7               0x4F7
