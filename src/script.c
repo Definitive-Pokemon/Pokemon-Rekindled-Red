e96536d5b5858c8104831d6e214712a9a51fbeeb
ElwinBran <elwinbran@gmail.com>
2025-01-14 15:48:32 +0100

    idem

--- src/script.c
+++ src/script.c
@@ -880,8 +880,8 @@ u8 ReturnBattleHouseLevel(void)
 void BattleHouseScaleLevelUp(void)
 {
     PlaySE(SE_PC_ON);
-    if(gSaveBlock1Ptr->BattleHouseData.levelGrowth < 12)
-        gSaveBlock1Ptr->BattleHouseData.levelGrowth++;
+    if(gSaveBlock1Ptr->battleHouseData.levelGrowth < 12)
+        gSaveBlock1Ptr->battleHouseData.levelGrowth++;
 }
 
 void UpdateBattleHouseStepCounter(void)
